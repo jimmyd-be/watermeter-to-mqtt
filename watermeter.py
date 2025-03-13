ba1bfc0bcdbf1687eab309be416126f296e6ab37
JimmyD-Be <dom.jimmy@gmail.com>
2025-03-13 15:42:23 +0100

    Revert "Try to move to rpi-lgpio"
    This reverts commit cb54bc7fc7b4f3c93c7657a72d19946426064727.

--- watermeter.py
+++ watermeter.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 import sys
+
+import RPi.GPIO as GPIO
 import time
 import os
 import paho.mqtt.client as mqtt
 import logging
-import lgpio  # Import the rpi-lgpio library
 
 # Watermeter stand (will only be used when there is nog meterstand_water.txt file)
+# global counter
 counter = 0
 
 # MQTT URL
@@ -14,70 +16,73 @@ mqtt_host = os.getenv('MQTT_HOST')
 mqtt_port = os.getenv('MQTT_PORT')
 mqtt_topic = os.getenv('MQTT_TOPIC')
 
-# Filename for persisting data locally
+# filename for persisting data local
 fileName = "/usr/src/app/config/meterstand_water.txt"
 
 # Pin property (pin 9 is GPIO 21)
 gpio_pin = 9
 
-# Set up logging
+# logging.basicConfig(filename='watermeter.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logger = logging.getLogger('watermeter')
 
 logger.info('Script started')
 
 # Open meterstand.txt file and read meterstand
-# If meterstand.txt does not exist, it will create the file and add the meterstand of Counter to it
+# If meterstand.txt does not exist it will create the file and add the meterstand of Counter to it
 fn = fileName
 if os.path.exists(fn):
-    with open(fn, "r+") as f:
-        inhoud = f.readline()
-        a, b, c = inhoud.split()
-        counter = int(c)
+    f = open(fn, "r+")
+    inhoud = f.readline()
+    a, b, c = inhoud.split()
+    counter = int(c)
 else:
-    with open(fn, "w") as f:
-        f.write('meterstand = ' + repr(counter))
+    f = open(fn, "w")
+    f.write('meterstand = ' + repr(counter))
+    f.close()
 
-# Initialize lgpio
-lgpio.gpiochip_open(0)  # Open the GPIO chip (0 is the default chip)
-lgpio.gpio_claim_output(gpio_pin, 0)  # Set the pin as input with pull-down
+# Board is pin nr, BMC is GPIO nr
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(gpio_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-# Callback function for interrupt
-def Interrupt(gpio_pin):
-    global counter
+
+# Functie  callback
+# This function will be called when an interrupt happens
+def Interrupt(channel):
     logger.info('Callback function called!')
-    time.sleep(0.05)  # Filter out false positives from power fluctuations
-    if lgpio.gpio_read(gpio_pin) == 0:
-        logger.warning('Quitting event handler because this was probably a false positive')
+    time.sleep(0.05)  # need to filter out the false positive of some power fluctuation
+    if GPIO.input(gpio_pin) == 0:
+        logger.warning('quitting event handler because this was probably a false positive')
         return
-
-    # Increment the counter
-    with open(fn, "r+") as f:
-        inbound = f.readline()
-        a, b, c = inbound.split()
-        counter = int(c) + 1
-
+    # counter will count every interrupt and add Couter with 0.5l (deler watermeter will be set on 10)
+    f = open(fn, "r+")
+    inbound = f.readline()
+    a, b, c = inbound.split()
+    counter = int(c)
+    counter = counter + 1
+    f.close()
     # Write counter to file
-    with open(fn, 'w') as f:
-        f.write('meterstand = ' + repr(counter))
-
-    # Send JSON to MQTT
+    f = open(fn, 'w')
+    f.write('meterstand = ' + repr(counter))
+    f.close()
+    # Send JSON TO MQTT
     try:
         client = mqtt.Client()
         client.connect(mqtt_host, int(mqtt_port), 60)
         client.publish(mqtt_topic, payload=counter, qos=0, retain=False)
         client.disconnect()
         logger.info("Watermeter counter = " + str(counter))
-    except Exception as e:
-        logger.error("Could not send message to MQTT server: " + str(e))
+    except:
+        logger.error("Could not sent message to MQTT server!")
 
-# Set up the interrupt event
-lgpio.gpio_claim_input(gpio_pin)  # Set the pin as input
-lgpio.gpio_set_event(gpio_pin, lgpio.RISING_EDGE, Interrupt)  # Set up the rising edge interrupt
+
+# Interrupt-Event toevoegen, sensor geeft een 0 en en bij detectie een 1
+# Bij detectie een 1 daarom check stijgende interrupt.
+GPIO.add_event_detect(gpio_pin, GPIO.RISING, callback=Interrupt, bouncetime=200)
 
 try:
     while True:
         time.sleep(0.2)
 except KeyboardInterrupt:
-    lgpio.gpiochip_close(0)  # Close the GPIO chip
+    GPIO.cleanup()
     logger.warning("\nBye")
